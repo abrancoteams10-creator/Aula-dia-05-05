c8f57d50adbc067249fd631f6c3bc6043ad847c7
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-05 15:03:50 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -6,4 +6,4 @@ st.title('Deputados Federais')
 st.image('https://www.rbsdirect.com.br/filestore/0/2/5/0/7/1/5_f03f083dd1c7353/5170520_d6f99c6fd82cd20.jpg?version=1575255600')
 
 df = pd.read_csv('deputados_2022.csv')
-
+st.dataframe(df)
