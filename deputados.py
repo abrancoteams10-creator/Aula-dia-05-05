de1218794e6e14a16b3b3de9a98cbabfcd3ad55b
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-05 15:07:57 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -7,3 +7,6 @@ st.image('https://www.rbsdirect.com.br/filestore/0/2/5/0/7/1/5_f03f083dd1c7353/5
 
 df = pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
+
+st.title('Buscador de deputados por Estado: ')
+Estado = st.text_input('Digite a sigla do estado: ')
