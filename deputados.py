1dba2c27e7177cea4787b518c8539d93739b2043
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-05 14:56:36 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Deputados Federais")
+st.title('Deputados Federais')
 
 st.image('https://www.rbsdirect.com.br/filestore/0/2/5/0/7/1/5_f03f083dd1c7353/5170520_d6f99c6fd82cd20.jpg?version=1575255600')
 df = pd.read_csv('deputados_2022.csv')
